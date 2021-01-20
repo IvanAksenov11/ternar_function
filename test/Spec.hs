208a66d065fb6c0cb6c40ab2faf96f2be9c122a4
IvanAksenov11 <ivenaksenov@yandex.ru>
2021-01-20 18:21:42 +0300

    testing both functions

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,5 +6,12 @@ import Ternar
 
 main :: IO ()
 main = do
+  print $ "testing ternar function"
   quickCheck first
   quickCheck second
+  print $ "testing mult function"
+  quickCheck commutative_prop
+  quickCheck associative_prop
+  quickCheck distributive_prop
+  quickCheck id_el
+  quickCheck zero_el
